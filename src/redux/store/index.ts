873f02463411aa5e3dec20f638fc78dded43b8ef
Suhas S Murthy <murthy.suhas@gmail.com>
2024-06-26 20:47:41 +0530

    toolkit store setup init

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,36 +1,15 @@
-import {
-  applyMiddleware,
-  compose,
-  legacy_createStore as createStore,
-} from 'redux';
-import { thunk, ThunkDispatch } from 'redux-thunk';
-import rootReducer from '../reducers';
-import { AppActionTypes } from '../types/types';
-import loggingMiddleware from '../middlewares/logging';
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
 
-// Define middleware array
-const middleware = [thunk, loggingMiddleware];
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "../reducers/userReducer";
+import { reposReducer } from "../reducers/reposReducer";
 
-const composeEnhancers =
-  (typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
 
-export const store = createStore(
-  rootReducer,
-  undefined,
-  composeEnhancers(applyMiddleware(...middleware))
-);
 
-export type ApplicationState = ReturnType<typeof rootReducer>;
-export type ApplicationDispatch = ThunkDispatch<
-  ApplicationState,
-  unknown,
-  AppActionTypes
->;
+export const store = configureStore({
+    reducer: {
+        users: usersReducer,
+        repos: reposReducer,
+    }
+})
+
